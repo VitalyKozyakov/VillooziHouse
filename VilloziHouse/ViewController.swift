c44fa35c406ded6bc3876b85fa9a938aef792409
VitalyK <vitaly3737@icloud.com>
2026-02-22 17:58:36 +0300

    Initial Commit

---
+++ VilloziHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VilloziHouse
+//
+//  Created by Виталий Козьяков on 22.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
