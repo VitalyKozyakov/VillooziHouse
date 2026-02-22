7939b51d6f09099315c1eb9fb5e0b4b584c62c64
VitalyK <vitaly3737@icloud.com>
2026-02-22 18:04:28 +0300

    Test

--- VilloziHouse/AppDelegate.swift
+++ VilloziHouse/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    private let window: UIWindow? = UIApplication.shared.windows.first
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
