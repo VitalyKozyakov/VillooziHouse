186b488d4e767b8bcc7948c0edcbe4cc8a576404
VitalyK <vitaly3737@icloud.com>
2026-02-22 18:23:10 +0300

    Настроили проект для использования без сториборда

--- VilloziHouse/SceneDelegate.swift
+++ VilloziHouse/SceneDelegate.swift
@@ -16,7 +16,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // 📚 Создаём UIWindow — главное окно приложения.
+        //    Передаём windowScene — привязываем окно к сцене.
+        let window = UIWindow(windowScene: windowScene)
+        
+        // Корневой вью контроллер, с которого строится все приложение
+        let vc = ViewController()
+        
+        window.rootViewController = vc
+        self.window = window
+        
+        // 📚 makeKeyAndVisible() — делает окно:
+        //    • key window — получает события касания
+        //    • visible — видимым на экране
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
